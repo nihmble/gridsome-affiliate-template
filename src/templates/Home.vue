1d2419dca79260c6d7f936e4bf8f5670b8eae5a2
nihmble <67880348+nihmble@users.noreply.github.com>
2020-09-26 00:06:17 -0600

    Initial commit

---
+++ src/templates/Home.vue
@@ -0,0 +1,21 @@
+<page-query>
+  query Home($id: ID!) {
+    data: home(id: $id) {
+      heroImage
+      heroText { code }
+    }
+  }
+</page-query>
+
+<template>
+  <Layout>
+    <img :src="$page.data.heroImage" alt="">
+    <p v-html="$page.data.heroText.code" />
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
