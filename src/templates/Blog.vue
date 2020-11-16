20510428f2bdb473268324f6fbbe27df747ea15c
Joshua Christensen <joshua@thedigitaldesign.com>
2020-11-15 20:00:07 -0700

    redesigning part of the blog page

--- src/templates/Blog.vue
+++ src/templates/Blog.vue
@@ -29,42 +29,45 @@
 <template>
   <Layout>
     <section class="max-w-7xl mt-4 sm:mt-12">
-      <div class="max-w-6xl mx-auto clearfix">
-        <ul id="blog-posts">
-          <li v-for="edge in $page.posts.edges" :key="edge.node.id" class="mb-8">
-            <article>
-              <header>
-                <h2 class="post-title-bar text-xl sm:text-4xl font-semibold">
-                  <a :href="edge.node.path" class="block h-full">
-                    {{ edge.node.title }}
+      <div class="max-w-6xl mx-auto flex flex-wrap">
+        <div class="w-full sm:w-3/4 sm:pr-8">
+          <ul id="blog-posts">
+            <li v-for="edge in $page.posts.edges" :key="edge.node.id" class="mb-12">
+              <article>
+                <header>
+                  <a :href="edge.node.path">
+                    <g-image v-if="edge.node.featured_image" :src="`/uploads/${edge.node.featured_image}`" :alt="`Featured image for ${edge.node.title}`" />
                   </a>
-                </h2>
-                <h4 class="post-subtitle-bar text-lg">
-                  {{ edge.node.published_date | prettyDate }}
-                </h4>
-              </header>
 
-              <a :href="edge.node.path">
-                <g-image v-if="edge.node.featured_image" :src="`/uploads/${edge.node.featured_image}`" :alt="`Featured image for ${edge.node.title}`" />
-              </a>
+                  <h1 class="post-title-bar text-xl sm:text-2xl font-semibold mt-4 px-4">
+                    <a :href="edge.node.path" class="block h-full">
+                      {{ edge.node.title }}
+                    </a>
+                  </h1>
+                  <h4 class="post-subtitle-bar text-md px-4">
+                    {{ edge.node.published_date | prettyDate }}
+                  </h4>
+                </header>
 
-              <div v-html="edge.node.excerpt" class="post-excerpt text-lg sm:text-md p-4 sm:px-4 sm:py-6" />
+                <div v-html="edge.node.excerpt" class="post-excerpt text-lg sm:text-md p-4 sm:px-4 sm:py-6" />
 
-              <div class="post-footer bg-gray-200 p-4 border-t-2 border-gray-400 flex">
-                <a
-                  :href="edge.node.path"
-                  class="inline-block mx-auto sm:mr-0 py-2 px-4 bg-gray-500 hover:bg-gray-400 border-b-4 border-gray-700 hover:border-gray-500 rounded flex-end"
-                >
-                  <span class="text-xl font-bold text-white">
-                    Read more...
-                  </span>
-                </a>
-              </div>
-            </article>
-          </li>
-        </ul>
+                <footer>
+                  <a
+                    :href="edge.node.path"
+                    class="inline-block mx-auto sm:mr-0 py-2 px-4"
+                  >
+                    <span class="text-md font-bold uppercase">
+                      Read more...
+                    </span>
+                  </a>
+                </footer>
+              </article>
+            </li>
+          </ul>
 
-        <Pager class="pagination" :info="$page.posts.pageInfo" />
+          <Pager class="pagination" :info="$page.posts.pageInfo" />
+        </div>
+        <div class="w-full sm:w-1/4 pr-8 bg-gray-400">asdf</div>
       </div>
     </section>
   </Layout>
@@ -99,40 +102,40 @@ export default {
 
     &:hover {
       @apply bg-gray-200;
-    } 
-  }
-}
-
-#blog-posts {
-  li {
-    border: solid 1px #999;
-
-    .post-title-bar {
-      background-color: #777;
-      border-bottom: solid 4px #555;
-      color: #fff;
-      padding: 0.25rem 1rem;
-
-      a {
-        display: block;
-        height: 100%;
-      }
-    }
-
-    .post-subtitle-bar {
-      background-color: #666;
-      color: #fff;
-      padding: 0.25rem 1rem;
     }
   }
 }
 
-@media (min-width: 640px) {
-  #blog-posts {
-    li {
-      max-width: 72%;
-      margin-right: 2rem;
-    }
-  }
-}
+// #blog-posts {
+//   li {
+//     border: solid 1px #999;
+
+//     .post-title-bar {
+//       background-color: #777;
+//       border-bottom: solid 4px #555;
+//       color: #fff;
+//       padding: 0.25rem 1rem;
+
+//       a {
+//         display: block;
+//         height: 100%;
+//       }
+//     }
+
+//     .post-subtitle-bar {
+//       background-color: #666;
+//       color: #fff;
+//       padding: 0.25rem 1rem;
+//     }
+//   }
+// }
+
+// @media (min-width: 640px) {
+//   #blog-posts {
+//     li {
+//       max-width: 72%;
+//       margin-right: 2rem;
+//     }
+//   }
+// }
 </style>
